0b44c9dbbf9e3f8e15e31635764fb87ebb4527cb
unknown <keithgibson58@gmail.com>
2023-04-03 01:13:16 -0600

    added timer and full functionality

--- script.js
+++ script.js
@@ -1,93 +1,122 @@
+
+//these are the array of questions to ask
 const quizData = [
     {
-        question: "which language runs in a web browser?",
-        a:"java",
-        b:"c",
-        c:"python",
-        d: "javascript",
-        correct: "d",
+        question: "What is the longest river in the world?",
+        a: "The Yangtze",
+        b: "The Amazon",
+        c: "The Nile",
+        d: "mississippi",
+        correct: "c",
     },
     {
-        question: "what does css stand for?",
-        a:"central Style Sheets",
-        b:"cascading style sheets",
-        c:"cascading simple sheets",
-        d:"Cars Suvs sailboats",
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
         correct: "b",
     },
     {
-        question: "What does html stand for?",
-        a:"hypertext markup language",
-        b:"hypertext markdown language",
-        c:"hyperloop machine language",
-        d:"helicopters terminals motorboats lambos",
+        question: "Variables in JavaScript are case sensitive",
+        a:"True",
+        b:"False",
+        c:"Sometimes",
+        d:"JavaScript doesn't have variables in it.",
         correct: "a",
     },
     {
-        question: "what year was javascript launched?",
-        a:"1996",
-        b:"1995",
-        c:"1994",
-        d:"none of the above",
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
         correct: "b",
     },
 ];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_answer = document.getElementById('a_answer')
+const b_answer = document.getElementById('b_answer')
+const c_answer = document.getElementById('c_answer')
+const d_answer = document.getElementById('d_answer')
+const submitBtn = document.getElementById('submit')
 
-const quiz = document.getElementById('quiz')
-const answerEL = document.querySelectorAll('.answer')
-const questionEL = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document. getElementById('submit')
 
 let currentQuiz = 0
 let score = 0
 
+//this should load the quiz
 loadQuiz()
-
 function loadQuiz() {
-
     deselectAnswers()
-
     const currentQuizData = quizData[currentQuiz]
-    console.log(currentQuizData.question)
-    questionEL.innertext = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
-
+    questionEl.innerText = currentQuizData.question
+    a_answer.innerText = currentQuizData.a
+    b_answer.innerText = currentQuizData.b
+    c_answer.innerText = currentQuizData.c
+    d_answer.innerText = currentQuizData.d
 }
 
-function deselectAnswers(){
-    answerEL.forEach(answerEL => answerEL.checked = false)
+//this function alows you to diselect
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
 }
 
-function getselected() {
+//this alows you to select
+function getSelected() {
     let answer
-    answerEL.forEach(answerEL => {
-        if(answerEL.checked){
-            answer= answerEL.id
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
         }
     })
     return answer
 }
 
+//adding an event listener for score and page reload
 
 submitBtn.addEventListener('click', () => {
-    const answer = getselected()
+    const answer = getSelected()
     if(answer) {
-        if(answer === quizData[currentQuiz].correct){
-            score++
-        }
-
-        currentQuiz++
-
-        if(currentQuiz < quizData.length){
-            loadQuiz()
-            
-        }
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
     }
 })
+
+let timeSecond = 30;
+const timeH = document.querySelector("h1");
+
+displayTime(timeSecond);
+
+const countDown = setInterval(() => {
+  timeSecond--;
+  displayTime(timeSecond);
+  if (timeSecond == 0 || timeSecond < 1) {
+    endCount();
+    clearInterval(countDown);
+  }
+}, 1000);
+
+function displayTime(second) {
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  timeH.innerHTML = `
+  ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}
+  `;
+}
+
+function endCount() {
+  timeH.innerHTML = "Time out";
+}
